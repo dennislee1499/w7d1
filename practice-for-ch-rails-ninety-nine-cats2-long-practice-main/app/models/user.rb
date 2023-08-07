6c18d257e9c28f351992c3a921bf18f9541818d8
elevenstand7 <elevenstand7@gmail.com>
2023-08-07 14:53:15 -0400

    up to generate_unique_session_token

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -1,3 +1,32 @@
 class User < ApplicationRecord
-    
+    validates :username, :session_token, presence:true, uniqueness:true
+    validates :password_digest, presence:true
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  private
+  def generate_unique_session_token
+    loop do
+      session_token = SecureRandom::urlsafe_base64(16)
+      return session_token unless User.exists?(session_token: session_token)
+    end
+  end
+
 end
