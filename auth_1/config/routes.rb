94a8fc7ffed4f550b53bf8080ca13bc9843962ac
Dennis Lee <dennislee1499@gmail.com>
2023-08-07 14:09:52 -0400

    starting auth

--- auth_1/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "users#new"
-  resources :users, only: %i(create new show)
-  resource :session, only: %i(create destroy new)
-end
--- auth_1/db/migrate/20220608183505_add_session_token_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddSessionTokenToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :session_token, :string, null: false
-    add_index :users, :session_token, unique: true
-  end
-end
