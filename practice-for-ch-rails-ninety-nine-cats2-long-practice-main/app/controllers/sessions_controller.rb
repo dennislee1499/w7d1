5bbe4851b8ea30a3c4aac5001e214ccfce510a0d
elevenstand7 <elevenstand7@gmail.com>
2023-08-07 16:09:47 -0400

    before Using the session

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username]
+      params[:user][:password]
+    )
+    session[:session_token] = @user.reset_session_token!
+    redirect_to cats_url
+  end
+
+end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
-    def new 
+    def new
         @user = User.new
-        render :new 
+        render :new
     end
 
-    private 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to users_url
+        else
+            render :new
+        end
+    end
+
+    private
     def user_params
         params.require(:user).permit(:username, :session_token, :password_digest)
     end
